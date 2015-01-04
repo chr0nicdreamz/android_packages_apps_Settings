32677d96b44dad8a8ee4105120abb2135599dc41
Danesh Mondegarian <daneshm90@gmail.com>
2015-01-03 18:34:37 -0700

    Settings : Add contributors word cloud
    Change-Id: I43dc37e7d164ae0a641e39bdcfbdaebad62c85fe
    (cherry picked from commit d206e5e81a7756fd77037e00142bd02a8810af7d)
    
    Conflicts:
    	Android.mk
    	res/xml/device_info_settings.xml

--- Android.mk
+++ Android.mk
@@ -36,6 +36,7 @@ LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res $(LOCAL_PATH)/../../../external/koush/Wi
 LOCAL_RESOURCE_DIR += packages/apps/PerformanceControl/res
 
 LOCAL_ASSET_DIR += packages/apps/PerformanceControl/assets
+LOCAL_ASSET_DIR := $(LOCAL_PATH)/assets
 
 LOCAL_JAVA_LIBRARIES += org.cyanogenmod.hardware
 
