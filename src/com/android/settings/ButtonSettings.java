7a7a8d71aa0c07e27ed11da3ce05efdcc7030963
scott <scott@beanstalk.com>
2015-02-12 20:59:43 -0700

    Settings: Add Volume Key Answer (2/2)
    Change-Id: Icccdcd1f1947323cfbd7483604e7a4d2f8a58521
    Signed-off-by: scott <scott@beanstalk.com>
    
    Conflicts:
    	res/values/cmr_strings.xml
    	res/xml/button_settings.xml
    	src/com/android/settings/ButtonSettings.java

--- src/com/android/settings/ButtonSettings.java
+++ src/com/android/settings/ButtonSettings.java
@@ -80,6 +80,7 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
     private static final String KEY_HOME_ANSWER_CALL = "home_answer_call";
     private static final String KEY_BLUETOOTH_INPUT_SETTINGS = "bluetooth_input_settings";
     private static final String NAVIGATION_BAR_TINT = "navigation_bar_tint";
+    private static final String KEY_VOLUME_ANSWER_CALL = "volume_answer_call";
 
     private static final String CATEGORY_POWER = "power_key";
     private static final String CATEGORY_HOME = "home_key";
@@ -133,6 +134,7 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
     private SwitchPreference mPowerEndCall;
     private SwitchPreference mHomeAnswerCall;
     private ColorPickerPreference mNavbarButtonTint;
+    private SwitchPreference mVolumeAnswerCall;
 
     private Handler mHandler;
 
@@ -149,6 +151,10 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
 
         // Home button answers calls.
         mHomeAnswerCall = (SwitchPreference) findPreference(KEY_HOME_ANSWER_CALL);
+
+	// Volume button answers calls.
+	mVolumeAnswerCall = (SwitchPreference) findPreference(KEY_VOLUME_ANSWER_CALL);
+	mVolumeAnswerCall.setOnPreferenceChangeListener(this);
 
         mHandler = new Handler();
 
@@ -422,6 +428,10 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
             mHomeAnswerCall.setChecked(homeButtonAnswersCall);
         }
 
+	// Volume button answers calls.
+	    mVolumeAnswerCall.setChecked((Settings.CMREMIX.getInt(getContentResolver(),
+	    Settings.CMREMIX.ANSWER_VOLUME_BUTTON_BEHAVIOR_ANSWER, 0) == 1));
+
     }
 
     private ListPreference initActionList(String key, int value) {
@@ -527,6 +537,11 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
             Settings.CMREMIX.putInt(getActivity().getContentResolver(),
                     Settings.CMREMIX.NAVIGATION_BAR_TINT, intHex);
             return true;
+	   } else if (preference == mVolumeAnswerCall) {
+	    boolean value = (Boolean) newValue;
+	    Settings.CMREMIX.putInt(getContentResolver(),
+		    Settings.CMREMIX.ANSWER_VOLUME_BUTTON_BEHAVIOR_ANSWER, value ? 1 : 0);
+	    return true;
         }
         return false;
     }
