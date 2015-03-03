4db3eeb49c28dd5f46e46f2bdbccdf5e286e3515
Dave Kessler <activethrasher00@gmail.com>
2015-03-03 14:10:33 -0700

    Settings: Statusbar greeting (2/2)
    AICPfy
    
    Change-Id: Idfb665132983b109852ae6ede999baf190183fe3
    
    Conflicts:
    	res/xml/status_bar_settings.xml
    	src/com/android/settings/cyanogenmod/StatusBarSettings.java

--- src/com/android/settings/cyanogenmod/StatusBarSettings.java
+++ src/com/android/settings/cyanogenmod/StatusBarSettings.java
@@ -15,8 +15,10 @@
 */
 package com.android.settings.cyanogenmod;
 
+import android.app.AlertDialog;
 import android.content.ContentResolver;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.net.ConnectivityManager;
@@ -29,6 +31,8 @@ import android.preference.PreferenceScreen;
 import android.preference.SwitchPreference;
 import android.provider.Settings;
 import android.text.TextUtils;
+import android.text.Spannable;
+import android.widget.EditText;
 import android.view.View;
 
 import com.android.settings.R;
@@ -54,6 +58,7 @@ public class StatusBarSettings extends SettingsPreferenceFragment
     private static final String MISSED_CALL_BREATH = "missed_call_breath";
     private static final String VOICEMAIL_BREATH = "voicemail_breath";
     private static final String KEY_STATUS_BAR_NETWORK_ARROWS= "status_bar_show_network_activity";
+    private static final String KEY_STATUS_BAR_GREETING = "status_bar_greeting";
 
 
     private PreferenceScreen mClockStyle;
@@ -61,6 +66,10 @@ public class StatusBarSettings extends SettingsPreferenceFragment
     private SwitchPreference mMissedCallBreath;
     private SwitchPreference mVoicemailBreath;
     private SwitchPreference mNetworkArrows;
+    private SwitchPreference mStatusBarGreeting;
+
+    private String mCustomGreetingText = "";
+
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -81,6 +90,13 @@ public class StatusBarSettings extends SettingsPreferenceFragment
         int networkArrows = Settings.CMREMIX.getInt(getContentResolver(),
                 Settings.CMREMIX.STATUS_BAR_SHOW_NETWORK_ACTIVITY, 1);
         updateNetworkArrowsSummary(networkArrows);
+
+        // Greeting
+        mStatusBarGreeting = (SwitchPreference) findPreference(KEY_STATUS_BAR_GREETING);
+        mCustomGreetingText = Settings.CMREMIX.getString(getActivity().getContentResolver(),
+                Settings.CMREMIX.STATUS_BAR_GREETING);
+        boolean greeting = mCustomGreetingText != null && !TextUtils.isEmpty(mCustomGreetingText);
+        mStatusBarGreeting.setChecked(greeting);
 
         mSMSBreath = (SwitchPreference) findPreference(SMS_BREATH);
         mMissedCallBreath = (SwitchPreference) findPreference(MISSED_CALL_BREATH);
@@ -136,6 +152,47 @@ public class StatusBarSettings extends SettingsPreferenceFragment
             return false;
         }
         return true;
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+       if (preference == mStatusBarGreeting) {
+           boolean enabled = mStatusBarGreeting.isChecked();
+           if (enabled) {
+                AlertDialog.Builder alert = new AlertDialog.Builder(getActivity());
+
+                alert.setTitle(R.string.status_bar_greeting_title);
+                alert.setMessage(R.string.status_bar_greeting_dialog);
+
+                // Set an EditText view to get user input
+                final EditText input = new EditText(getActivity());
+                input.setText(mCustomGreetingText != null ? mCustomGreetingText : "Welcome to cmRemiX");
+                alert.setView(input);
+                alert.setPositiveButton(getResources().getString(R.string.ok), new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                        String value = ((Spannable) input.getText()).toString();
+                        Settings.CMREMIX.putString(getActivity().getContentResolver(),
+                                Settings.CMREMIX.STATUS_BAR_GREETING, value);
+                        updateCheckState(value);
+                    }
+                });
+                alert.setNegativeButton(getResources().getString(R.string.cancel), new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                        // Canceled.
+                    }
+                });
+
+                alert.show();
+            } else {
+                Settings.CMREMIX.putString(getActivity().getContentResolver(),
+                        Settings.CMREMIX.STATUS_BAR_GREETING, "");
+            }
+        }
+        return super.onPreferenceTreeClick(preferenceScreen, preference);
+    }
+
+    private void updateCheckState(String value) {
+        if (value == null || TextUtils.isEmpty(value)) mStatusBarGreeting.setChecked(false);
     }
 
     @Override
