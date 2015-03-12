54eacc2860c0e5967d168e1617fdd62022982b91
Jorge Ruesga <jorge@ruesga.com>
2015-03-11 22:15:15 -0700

    settings: prevent NPE in DisplaySettings
    Change-Id: If70f000bebfc7d072d060d71ee43e6b843167272
    JIRA: NIGHTLIES-830
    Signed-off-by: Jorge Ruesga <jorge@ruesga.com>
    
    Conflicts:
    	src/com/android/settings/DisplaySettings.java

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -215,7 +215,8 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
         }
 
         mTapToWake = (SwitchPreference) findPreference(KEY_TAP_TO_WAKE);
-        if (displayPrefs != null && !mCmHardwareManager.isSupported(FEATURE_TAP_TO_WAKE)) {
+        if (displayPrefs != null && mTapToWake != null
+                && !mCmHardwareManager.isSupported(FEATURE_TAP_TO_WAKE)) {
             displayPrefs.removePreference(mTapToWake);
             mTapToWake = null;
         }
@@ -246,10 +247,11 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
             mSweepToSleep = null;
         }
 
+        Preference proximityWake = findPreference(KEY_PROXIMITY_WAKE);
         boolean proximityCheckOnWake = getResources().getBoolean(
                 com.android.internal.R.bool.config_proximityCheckOnWake);
-        if (displayPrefs != null && !proximityCheckOnWake) {
-            displayPrefs.removePreference(findPreference(KEY_PROXIMITY_WAKE));
+        if (displayPrefs != null && proximityWake != null && !proximityCheckOnWake) {
+            displayPrefs.removePreference(proximityWake);
             Settings.System.putInt(getContentResolver(), Settings.System.PROXIMITY_ON_WAKE, 1);
         }
 
