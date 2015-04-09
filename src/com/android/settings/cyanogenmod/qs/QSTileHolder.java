a3aef7066f2b3d8e7bf03e3d4d658093ada3c7f4
fusionjack <dogfight60-fusionjack@yahoo.de>
2015-04-09 10:19:35 -0700

    Settings: SlimAction as QuickTile (2/2)
    Configure which Slim actions you want to use as tile and then add the 'SlimAction' tile.
    Single press will switch between Slim actions and long press will execute the Slim action. If the tile has only one Slim action, single press will execute the action.
    
    Change-Id: I6a01ca065801a2ceabf1b0c21dc9d9c0a0289af2
    
    Conflicts:
    	res/xml/notification_drawer_settings.xml
    
    Conflicts:
    	res/values/cmr_arrays.xml
    	res/xml/notification_drawer_settings.xml
    
    Conflicts:
    	res/xml/notification_drawer_settings.xml
    	src/com/android/settings/cyanogenmod/qs/QSTileHolder.java

--- src/com/android/settings/cyanogenmod/qs/QSTileHolder.java
+++ src/com/android/settings/cyanogenmod/qs/QSTileHolder.java
@@ -145,10 +145,6 @@ public class QSTileHolder {
                 resourceName = "ic_qs_screen_timeout_vector";
                 stringId = R.string.qs_tile_screen_timeout;
                 break;
-            case QSConstants.TILE_LIVE_DISPLAY:
-                resourceName ="ic_livedisplay_day";
-                stringId = R.string.live_display_title;
-                break;
             case QSConstants.TILE_SCREENSHOT:
                 resourceName = "ic_qs_screenshot";
                 stringId = R.string.qs_screenshot_tile;
@@ -204,6 +200,14 @@ public class QSTileHolder {
             case QSConstants.TILE_AMBIENT_DISPLAY:
                 resourceName = "ic_qs_doze";
                 stringId = R.string.qs_ambient_display_tile;
+                break;
+            case QSConstants.TILE_LIVE_DISPLAY:
+                resourceName ="ic_livedisplay_day";
+                stringId = R.string.live_display_title;
+                break;
+            case QSConstants.TILE_SLIMACTION:
+                resourceName = "ic_qs_reboot_recovery";
+                stringId = R.string.qs_tile_slimaction;
                 break;
             default:
                 return null;
