88df6a1d5ae633c81f794f747a38b26825e55ee9
Altaf-Mahdi <altaf.mahdi@gmail.com>
2015-04-09 18:08:08 -0700

    QS: add Ambient display tile * add Ambient display settings activity for long press on tile
    Drawables thanks to Alexander Westphal <westphal.alex94@gmail.com>
    
    Change-Id: Id52c668d7d73d66feac7282861459bd15972f4ac
    
    Conflicts:
    	res/values/custom_strings.xml
    	src/com/android/settings/Settings.java
    	src/com/android/settings/SettingsActivity.java
    	src/com/android/settings/cyanogenmod/qs/QSTileHolder.java
    
    Conflicts:
    	AndroidManifest.xml
    	src/com/android/settings/Settings.java
    	src/com/android/settings/SettingsActivity.java
    	src/com/android/settings/cyanogenmod/qs/QSTileHolder.java
    
    Conflicts:
    	src/com/android/settings/Settings.java
    	src/com/android/settings/SettingsActivity.java
    
    Conflicts:
    	res/values/cm_strings.xml

--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -115,6 +115,7 @@ import com.android.settings.notification.ZenModeSettings;
 import com.android.settings.print.PrintJobSettingsFragment;
 import com.android.settings.print.PrintSettingsFragment;
 import com.android.settings.sim.SimSettings;
+import com.android.settings.cmremix.AmbientSettings;
 import com.android.settings.tts.TextToSpeechSettings;
 import com.android.settings.users.UserSettings;
 import com.android.settings.voice.VoiceInputSettings;
@@ -326,7 +327,8 @@ public class SettingsActivity extends Activity
             LiveDisplay.class.getName(),
             QSTiles.class.getName(),
             DisplayRotation.class.getName(),
-            ExpandedDesktopPreferenceFragment.class.getName()
+            ExpandedDesktopPreferenceFragment.class.getName(),
+            AmbientSettings.class.getName()
     };
 
 
