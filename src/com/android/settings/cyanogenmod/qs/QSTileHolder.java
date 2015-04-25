6902cf383902e4d1d0dd738a07c324c8b6b6bb98
ZION959 <ziontran@gmail.com>
2015-04-25 15:39:10 -0700

    [Squash] Revert Slim stuffs
    Revert: Settings: Slim Actions to Shortcuts
    
    Change-Id: I8c9a524f3628c47e882347225372ff065b4f2b94
    
    Conflicts:
    	res/values/cr_arrays.xml
    
    Conflicts:
    	res/values/cmr_arrays.xml
    	res/values/cmr_strings.xml
    
    Conflicts:
    	res/values/cmr_arrays.xml
    	res/values/cmr_strings.xml (reverted from commit 8795806a9331b2ecea82f35eda92ed88886fe06e)
    Revert: Settings: SlimAction as QuickTile (2/2)
    
    Configure which Slim actions you want to use as tile and then add the 'SlimAction' tile.
    Single press will switch between Slim actions and long press will execute the Slim action. If the tile has only one Slim action, single press will execute the action.
    
    Change-Id: I6a01ca065801a2ceabf1b0c21dc9d9c0a0289af2
    
    Conflicts:
    	res/xml/notification_drawer_settings.xml
    
    Conflicts:
    	res/values/cmr_arrays.xml
    	res/xml/notification_drawer_settings.xml
    
    Conflicts:
    	res/xml/notification_drawer_settings.xml
    	src/com/android/settings/cyanogenmod/qs/QSTileHolder.java (reverted from commit a3aef7066f2b3d8e7bf03e3d4d658093ada3c7f4)
    Revert:  [2/2] Settings: Configurable overflow menu button
    
    Forward port from SlimBean. It was part of the hardware key custom rebinding.
    
    (cherry picked from commit 10e658f258a07f8aa2961c2eca266f08b7ff47e4)
    
    Conflicts:
    	res/values/cmr_strings.xml
    	src/com/android/settings/ButtonSettings.java (reverted from commit b96ffd5f6ac36930c189d64c43e8a6bb9a48d81d)
    
    Change-Id: I5b1edf45c30beb1b23f115235b72a68a52af0cfa
    Revert "Fix NPE on overflow due to removal of HW Keys category"
    
    This reverts commit 558e4704b56a527f3da2b6fb6ba7a6db1d24ba49.
    
    Change-Id: I24d10e29f739d0a9cdf80cbbddee550877d69d55

--- src/com/android/settings/cyanogenmod/qs/QSTileHolder.java
+++ src/com/android/settings/cyanogenmod/qs/QSTileHolder.java
@@ -145,6 +145,10 @@ public class QSTileHolder {
                 resourceName = "ic_qs_screen_timeout_vector";
                 stringId = R.string.qs_tile_screen_timeout;
                 break;
+            case QSConstants.TILE_LIVE_DISPLAY:
+                resourceName ="ic_livedisplay_day";
+                stringId = R.string.live_display_title;
+                break;
             case QSConstants.TILE_SCREENSHOT:
                 resourceName = "ic_qs_screenshot";
                 stringId = R.string.qs_screenshot_tile;
@@ -200,14 +204,6 @@ public class QSTileHolder {
             case QSConstants.TILE_AMBIENT_DISPLAY:
                 resourceName = "ic_qs_doze";
                 stringId = R.string.qs_ambient_display_tile;
-                break;
-            case QSConstants.TILE_LIVE_DISPLAY:
-                resourceName ="ic_livedisplay_day";
-                stringId = R.string.live_display_title;
-                break;
-            case QSConstants.TILE_SLIMACTION:
-                resourceName = "ic_qs_reboot_recovery";
-                stringId = R.string.qs_tile_slimaction;
                 break;
             case QSConstants.TILE_MUSIC:
                 resourceName = "ic_qs_media_play";
