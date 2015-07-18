d475f54e9666e129380c4cec66b6edbe29c97d3c
Davor Bertovic <lordclockan@gmail.com>
2015-07-17 20:45:45 -0700

    Fix Adaway once and for all.
    Change-Id: I513cd8ad7be6f2e3aaeb9abaa34b082e8e762fbc
    
    Conflicts:
    	res/values/cmr_strings.xml

--- src/com/android/settings/cmremix/hfm/HfmHelpers.java
+++ src/com/android/settings/cmremix/hfm/HfmHelpers.java
@@ -62,12 +62,12 @@ public final class HfmHelpers {
         File altHosts = new File("/etc/hosts.alt");
         File altOrigHosts = new File("/etc/hosts.alt_orig");
         File hosts = new File("/etc/hosts");
-        if (applyWhitelist || ! altOrigHosts.exists()) {
+        if (applyWhitelist || !altOrigHosts.exists()) {
             SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
             Set<String> hfmWhitelistSet = sharedPreferences.getStringSet("hfm_whitelist", null);
             if (hfmWhitelistSet != null) {
                 ArrayList<String> hfmWhitelist = new ArrayList<String>(hfmWhitelistSet);
-                if ( ! hfmWhitelist.isEmpty() ) {
+                if ( !hfmWhitelist.isEmpty() ) {
                     HfmWhitelist.applyWhitelist(context, hfmWhitelist);
                 }
             }
@@ -76,8 +76,9 @@ public final class HfmHelpers {
             boolean adsDisabled = Settings.CMREMIX.getInt(context.getContentResolver(), Settings.CMREMIX.HFM_DISABLE_ADS, 0) == 1;
             if (adsDisabled && areFilesDifferent(hosts, altHosts)) {
                 copyFiles(altHosts, hosts);
-            } else if ( ! adsDisabled && areFilesDifferent(hosts, defHosts)) {
+            } else if ( !adsDisabled && areFilesDifferent(hosts, defHosts)) {
                 copyFiles(defHosts, hosts);
+                deleteFiles(defHosts, altHosts);
             }
         }
         catch(IOException e)
@@ -132,6 +133,16 @@ public final class HfmHelpers {
                        + " && rm -f " + dstFile.getAbsolutePath()
                        + " && cp -f " + srcFile.getAbsolutePath() + " " + dstFile.getAbsolutePath()
                        + " && chmod 644 " + dstFile.getAbsolutePath()
+                       + " ; mount -o ro,remount /system";
+            RunAsRoot(cmd);
+        }
+    }
+
+    public static void deleteFiles(File srcFile, File dstFile) throws IOException {
+        if (srcFile.exists() && dstFile.exists()) {
+            String cmd = "mount -o rw,remount /system"
+                       + " && rm -f " + srcFile.getAbsolutePath()
+                       + " && rm -f " + dstFile.getAbsolutePath()
                        + " ; mount -o ro,remount /system";
             RunAsRoot(cmd);
         }
