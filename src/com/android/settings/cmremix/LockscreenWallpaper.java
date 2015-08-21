2d36b78f6a60c509d7d14e441f3650b4eaefa49c
ZION959 <ziontran@gmail.com>
2015-08-20 23:44:12 -0700

    lockscreenwallpapers: derp fixed

--- src/com/android/settings/cmremix/LockscreenWallpaper.java
+++ src/com/android/settings/cmremix/LockscreenWallpaper.java
@@ -15,7 +15,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  */
-package com.android.settings.slim;
+package com.android.settings.cmremix;
 
 import android.app.Activity;
 import android.app.WallpaperManager;
