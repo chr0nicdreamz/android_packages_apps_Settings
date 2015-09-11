9dcc8e3f76001c7272884328f39a7bc24c4e94f0
Roman Birg <roman@cyngn.com>
2015-09-11 15:55:19 -0700

    Settings: reset root option selection as necessary
    When root is disabled and trying to enable root, a warning dialog pops
    up. If the user selects cancel, we need to reset the property to the
    actual selection since it hasn't changed.
    
    Change-Id: I6fa1564d86b8473090bbad1b65544481528f0946
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- src/com/android/settings/DevelopmentSettings.java
+++ src/com/android/settings/DevelopmentSettings.java
@@ -2111,6 +2111,9 @@ public class DevelopmentSettings extends SettingsPreferenceFragment
         } else if (dialog == mUpdateRecoveryDialog) {
             updateUpdateRecoveryOptions();
             mUpdateRecoveryDialog = null;
+        } else if (dialog == mRootDialog) {
+            updateRootAccessOptions();
+            mRootDialog = null;
         }
     }
 
